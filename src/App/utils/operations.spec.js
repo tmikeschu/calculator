c3fd380955c96492ed67297905c44b5bd33a83cf
Mike Schutte <schutte@quikly.com>
2017-11-15 09:26:06 -0500

    Boost test coverage for utils

--- src/App/utils/operations.spec.js
+++ src/App/utils/operations.spec.js
@@ -5,23 +5,43 @@ describe("static math functions", () => {
     it("adds two numbers", () => {
       expect(ops.add(1, 2)).toEqual(3)
     })
+
+    it("adds multiple numbers", () => {
+      expect(ops.add(1, 2, 4)).toEqual(7)
+      expect(ops.add(1, 2, 10, -8)).toEqual(5)
+    })
   })
 
   describe("subtract", () => {
     it("subtracts the second number from the first", () => {
       expect(ops.subtract(1, 2)).toEqual(-1)
     })
+
+    it("subtracts multiple numbers from the first", () => {
+      expect(ops.subtract(1, 2, 3)).toEqual(-4)
+      expect(ops.subtract(1, 2, 3, -8)).toEqual(4)
+    })
   })
 
   describe("divide", () => {
-    it("subtracts the second number from the first", () => {
+    it("divides the first number by the second", () => {
       expect(ops.divide(6, 3)).toEqual(2)
+    })
+
+    it("divides the first number by multiple numbers", () => {
+      expect(ops.divide(6, 3, 2)).toEqual(1)
+      expect(ops.divide(20, 5, 2)).toEqual(2)
     })
   })
 
   describe("multiply", () => {
-    it("subtracts the second number from the first", () => {
+    it("multiplies the first number by the second", () => {
       expect(ops.multiply(6, 3)).toEqual(18)
+    })
+
+    it("multiplies the first number by multiple numbers", () => {
+      expect(ops.multiply(6, 3, 2)).toEqual(36)
+      expect(ops.multiply(6, 3, 2, -2)).toEqual(-72)
     })
   })
 })
